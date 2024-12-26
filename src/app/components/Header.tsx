dc32891f941e6bced75f35e7b7b70595d4e32241
Haniya-Shuja <haniyashuja786@gmail.com>
2024-12-26 10:06:13 -0800

    first commit

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='flex justify-between  shadow-2xl W-50 bg-gray-300'>
+        <div>app</div>
+    
+<div className='flex gap-4 cursor-pointer'>
+ 
+   </div>
+
+   </div>
+  )
+}
+
+export default Header
